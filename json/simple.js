fe3b81ffb192576c337d7cc6929ff5b86780f28c
divya gawade <gawadedivya15@gmail.com>
2024-05-11 16:25:48 +0530

    cheng page

---
+++ json/simple.js
@@ -0,0 +1,13 @@
+// Define a JSON object
+var person = {
+    name: "diuu",
+    age: 21,
+    city: "pune"
+  }
+  
+  // Convert the JSON object to a string
+  // var jsonString = JSON.stringify(person);
+  
+  // Print the JSON string
+  console.log(person.age);
+  
